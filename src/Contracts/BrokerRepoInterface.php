8efc8080cd4213dbee95cbbb3f401e31ffdf09a9
Abdellah Bouskine <Abdellah.Bouskine@terabyte-software.com>
2023-12-05 10:29:01 +0000

    add routing key to the publishMessageToExchange Updated BrokerRepoInterface.php

--- src/Contracts/BrokerRepoInterface.php
+++ src/Contracts/BrokerRepoInterface.php
@@ -10,7 +10,7 @@ interface BrokerRepoInterface
     public function connect();
     public function consumeMessageFromQueue(string $consumeQueue, callable $callback): void;
     public function publishMessageToQueue(AMQPMessage $message, string $publishQueue);
-    public function publishMessageToExchange(AMQPMessage $message, string $exchange);
+    public function publishMessageToExchange(AMQPMessage $message, string $exchange, string $routingKey = "");
     public function publishBulkMessagesToQueue(Collection $messages, string $queue);
     public function publishBulkMessagesToExchange(Collection $messages, string $exchange);
     public function status();
